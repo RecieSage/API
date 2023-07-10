a3dd1b526406e58df51567bb8eaae4626ee5c6e1
DustinDEV2more <public@dustin-dm.de>
2023-07-10 20:56:07 +0200

    add database creation file

---
+++ DatabaseUpdateScripts/0001.sql
@@ -0,0 +1,30 @@
+CREATE TABLE DB_Scripts (
+	script_id int NOT NULL,
+	success bit NOT NULL,
+	output nvarchar(MAX),
+	last_execution datetime NOT NULL
+)
+
+CREATE TABLE Ingredient (
+	id int NOT NULL UNIQUE,
+	name nvarchar(50) NOT NULL,
+	unit nvarchar(50) NOT NULL
+	PRIMARY KEY (id)
+)
+
+CREATE TABLE Recipe (
+	id int NOT NULL UNIQUE,
+	name nvarchar(50) NOT NULL,
+	instructions nvarchar(1500) NOT NULL
+	PRIMARY KEY (id)
+)
+
+CREATE TABLE Recipe_Ingredient (
+	id int NOT NULL UNIQUE,
+	recipe_id int NOT NULL,
+	ingredient_id int NOT NULL,
+	amount int NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (recipe_id) REFERENCES Recipe(id),
+	FOREIGN KEY (ingredient_id) REFERENCES Ingredient(id)
+)
